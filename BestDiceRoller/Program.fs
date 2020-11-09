32986a79c4f3755b0bf0821211d10a4507d77820
Toby <toby.lawrance@gmail.com>
2020-11-09 00:32:45 +0000

    Add project files.

---
+++ BestDiceRoller/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Discord
+open Discord.WebSocket
+
+let createClient = new DiscordSocketClient
+
+
+let async log msg = printfn "%s" (string msg)
+
+let async mainAsync = 
+    let _client = createClient
+    _client.add_Log log
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ BestDiceRollerShell/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace BestDiceRollerShell
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            // Remember to keep token private or to read it from an 
+            // external source! In this case, we are reading the token 
+            // from an environment variable. If you do not know how to set-up
+            // environment variables, you may find more information on the 
+            // Internet or by using other methods such as reading from 
+            // a configuration.
+            await _client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
